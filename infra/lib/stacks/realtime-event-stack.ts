370fc0667898994f57d88752d59ad205481545f4
tamaco489 <tamaco489@gmail.com>
2026-06-02 23:26:37 +0900

    chore: AppSync チャンネル変更と移行メモ削除

--- infra/lib/stacks/realtime-event-stack.ts
+++ infra/lib/stacks/realtime-event-stack.ts
@@ -53,7 +53,7 @@ export class RealtimeEventStack extends cdk.Stack {
       table: dynamoDbTable.table,
       channelNamespace: appSyncApi.channelNamespace,
       appSyncUrl: `https://${appSyncApi.api.httpDns}`,
-      appSyncChannel: "default/events",
+      appSyncChannel: "tickets/orders",
       appSyncApiKey: appSyncApi.api.apiKeys["Default"].attrApiKey,
       lambdaMemorySize: props.config.lambdaMemorySize,
       artifactsBucketName: props.config.artifactsBucketName,
